bb604c1c378e2baed654abf1eec23ad5d2272eab
JeffRisberg <jrisberg@aol.com>
2018-04-05 22:58:56 -0700

    updated to webpack 4

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,26 @@
 module.exports = {
-  entry: './app/main.js',
-  output: {
-    path: __dirname + "/dist",
-    filename: "bundle.js"
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader'
-      }
-    ]
-  },
-  resolve: {
-    alias: {
-      vue$: 'vue/dist/vue.esm.js'
+    mode: 'development',
+    entry: './app/main.js',
+    output: {
+        path: __dirname + "/dist",
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            }
+        ]
+    },
+    resolve: {
+        alias: {
+            vue$: 'vue/dist/vue.esm.js'
+        }
     }
-  }
 }
